cd75d0878b7817743036aee37f2437a9860488f1
gbrewer <greg@gsbrewer.com>
2024-09-02 08:33:40 -0700

    Changed a few device names and added a few device names  to Regency.csv for completeness (Devices were not used in MM but were generating warnings because they werent in the database).

--- Contents/Server Plugin/MM_VersionInfo.py
+++ Contents/Server Plugin/MM_VersionInfo.py
@@ -1,4 +1,4 @@
-MM_UploadTime = "08/20/2024 03:52:51 PM"
+MM_UploadTime = "09/02/2024 08:31:38 AM"
 # MM_UploadTime above must be first line as it is processed during upload routine (CopyToStaging)
 # Etc...
 # MM_Name =
